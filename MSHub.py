f94cfc791682ecab36482437f16b6a1b05e66ffd
cukormentescukorka <suhai.abris@gmail.com>
2023-02-28 01:00:47 +0100

    Update start impl

--- MSHub.py
+++ MSHub.py
@@ -15,8 +15,21 @@ import subprocess
 import copy
 import sys
 import zipfile
+import base64
+import requests
 
 
+#check for updates
+url = 'https://api.github.com/repos/{repo_name}/contents/{path_to_file}'
+req = requests.get(url)
+if req.status_code == requests.codes.ok:
+    req = req.json()  # the response is a JSON
+    # req is now a dict with keys: name, encoding, url, size ...
+    # and content. But it is encoded with base64.
+    content = base64.decodestring(req['content'])
+    print(content)
+else:
+    print('Version file was not found.')
 
 
 
